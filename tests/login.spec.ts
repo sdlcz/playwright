c161ae3899e87e2a25225138667907edb22b4265
sdlcz <stantonsdlc@gmail.com>
2025-01-02 12:04:19 +0000

    Updated login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/loginPage';
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-});
+// test.beforeEach(async ({ page }) => {
+//   await page.goto('https://www.saucedemo.com/');
+// });
 
 const acceptedUsernames = [
 'standard_user', 
@@ -21,13 +22,20 @@ test('has title', async ({ page }) => {
 });
 
 test('Login button with incorrect credentials', async ({ page }) => {
-  await page.getByPlaceholder('Username').fill('John');
-  await page.getByPlaceholder('Password').fill('testpassword');
-  await page.getByRole('button', { name: 'Login' }).click();
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login('John', 'testpassword');
+  const error = await loginPage.getErrorMessage();
+  expect(error).toContain('Epic sadface: Username and password do not match any user in this service');
+    
+  
+  // await page.getByPlaceholder('Username').fill('John');
+  // await page.getByPlaceholder('Password').fill('testpassword');
+  // await page.getByRole('button', { name: 'Login' }).click();
 
-  const invalidCredentialsError = page.locator('//*[@id="login_button_container"]/div/form/div[3]/h3');
-  await expect(invalidCredentialsError).toBeVisible();
-  await expect(invalidCredentialsError).toContainText('Epic sadface: Username and password do not match any user in this service');
+  // const invalidCredentialsError = page.locator('//*[@id="login_button_container"]/div/form/div[3]/h3');
+  // await expect(invalidCredentialsError).toBeVisible();
+  // await expect(invalidCredentialsError).toContainText('Epic sadface: Username and password do not match any user in this service');
 });
 
 test('Login button with correct credentials', async ({ page }) => { 
