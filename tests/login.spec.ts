0861ad334de622fef6f1cafbc831fd607bdfcc8a
sdlcz <stantonsdlc@gmail.com>
2025-01-02 13:21:57 +0000

    Fixed correct credentials

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,7 +4,7 @@ import { LoginPage } from '../pages/loginPage';
 test.beforeEach(async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 });
-
+  
 const acceptedUsernames = [
 'standard_user', 
 'locked_out_user',
@@ -15,9 +15,7 @@ const acceptedUsernames = [
 ] 
 
 const acceptedPassword = 'secret_sauce';
-
 test('has title', async ({ page }) => {
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Swag Labs/);
 });
 
@@ -29,12 +27,11 @@ test('Login button with incorrect credentials', async ({ page }) => {
 });
 
 test('Login button with correct credentials', async ({ page }) => { 
-  await page.getByPlaceholder('Username').fill(acceptedUsernames[0]);
-  await page.getByPlaceholder('Password').fill(acceptedPassword);
-  await page.getByRole('button', { name: 'Login' }).click();
-
+  const loginPage = new LoginPage(page);
+  await loginPage.login(acceptedUsernames[0], acceptedPassword);
   const pageTitle = page.locator('//*[@id="header_container"]/div[2]/span');
   await expect(pageTitle).toBeVisible();
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
 
 
